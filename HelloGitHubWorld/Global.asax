d90f47fe64112137570d2108956900495b8655e9
Jelle B <Jelle B@DESKTOP-25SD0N6>
2017-07-31 15:43:29 +0200

    Add project files.

---
+++ HelloGitHubWorld/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloGitHubWorld.MvcApplication" Language="C#" %>
---
+++ HelloGitHubWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloGitHubWorld.Startup))]
+namespace HelloGitHubWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
